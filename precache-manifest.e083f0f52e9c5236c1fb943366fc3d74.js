949dca95dca6aa6fb088cbdc6d8820e3aa4004d0
Efraim A. Lorenzana <efraim.lorenzana@gmail.com>
2020-09-07 17:41:29 +0800

    Updates

---
+++ precache-manifest.e083f0f52e9c5236c1fb943366fc3d74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5f15ac5ebcec6d15aeb",
+    "url": "/static/css/main.fb73658a.chunk.css"
+  },
+  {
+    "revision": "d5f15ac5ebcec6d15aeb",
+    "url": "/static/js/main.d5f15ac5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "00a28df9f6ee050de44a",
+    "url": "/static/js/2.00a28df9.chunk.js"
+  },
+  {
+    "revision": "b8d7d4231c488be3c6998ec585fa0168",
+    "url": "/static/media/sprite.b8d7d423.svg"
+  },
+  {
+    "revision": "80efc2a80113abb68750d7d27594423c",
+    "url": "/static/media/loading.80efc2a8.gif"
+  },
+  {
+    "revision": "5cc61138ecc0f4a7b64de822f3180999",
+    "url": "/static/media/main.5cc61138.jpg"
+  },
+  {
+    "revision": "dfb5dd855c244918fe50d7e5e3af2219",
+    "url": "/index.html"
+  }
+];
